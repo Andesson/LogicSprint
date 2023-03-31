3ace5e6ab4b5dacdf2103d6d10ff2a882db973ea
Andesson lins <andessonlins93@gmail.com>
2023-03-31 18:57:02 -0300

    commit

---
+++ src/main/java/com/logiclabs/logicsprint/LogicsprintApplication.java
@@ -0,0 +1,13 @@
+package com.logiclabs.logicsprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicsprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicsprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logiclabs/logicsprint/LogicsprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logiclabs.logicsprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicsprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
